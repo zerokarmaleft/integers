c35e1a7eb7417ac0ec1e7429ef47da199dbda300
Edward Cho <zerokarmaleft@gmail.com>
2012-09-18 23:45:07 -0500

    use ToolRunner for options parsing

--- src/main/java/integers/MaxInteger.java
+++ src/main/java/integers/MaxInteger.java
@@ -1,21 +1,28 @@
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class MaxInteger {
+public class MaxInteger extends Configured implements Tool {
 
-    public static void main(String[] args) throws Exception {
+    @Override
+    public int run(String[] args) throws Exception {
         if (args.length != 2) {
-            System.err.println("Usage: MaxInteger <input path> <output path>");
-            System.exit(-1);
+            System.err.printf(
+                "Usage: %s [options] <input path> <output path>",
+                getClass().getSimpleName());
+            ToolRunner.printGenericCommandUsage(System.err);
+
+            return -1;
         }
 
-        Job job = new Job();
-        job.setJarByClass(MaxInteger.class);
-        job.setJobName("Max integer");
+        Job job = new Job(getConf(), "Max integer");
+        job.setJarByClass(getClass());
 
         TextInputFormat.addInputPath(job, new Path(args[0]));
         TextOutputFormat.setOutputPath(job, new Path(args[1]));
@@ -26,6 +33,11 @@ public class MaxInteger {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(LongWritable.class);
 
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int result = ToolRunner.run(new MaxInteger(), args);
+        System.exit(result);
     }
 }
